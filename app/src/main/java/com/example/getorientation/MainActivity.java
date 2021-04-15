e6fd30a84484399e424b24c16feead770bf67f2b
박승준 <qtw32@naver.com>
2021-04-15 21:42:20 +0900

    comapss with two sensors

--- app/src/main/java/com/example/getorientation/MainActivity.java
+++ app/src/main/java/com/example/getorientation/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
    // TextView txtAzimuth, txtPitch, txtRoll;
+    AzimuthView azimuthView;
     SensorManager sensorManager;
     Sensor magSensor, accSensor;
     SensorEventListener listener;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 /*        txtAzimuth = findViewById(R.id.txtAzimuth);
         txtPitch = findViewById(R.id.txtPitch);
         txtRoll = findViewById(R.id.txtRoll);  */
+        azimuthView = findViewById(R.id.azimuthView);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -64,9 +66,14 @@ public class MainActivity extends AppCompatActivity {
 
                     SensorManager.getOrientation(R, values);
 
+/*                    if((int) radian2Degree(values[0]) == 180 || radian2Degree(values[0]) ==180) {
+                        values[0] = 0;
+                    } */
 
+                    azimuthView.azimuth = (int)radian2Degree(values[0]);
+                    azimuthView.invalidate();
 
-                    if((int) radian2Degree(values[0]) == 180) {
+/*                    if((int) radian2Degree(values[0]) == 180) {
 
                         Toast.makeText(MainActivity.this, "180", Toast.LENGTH_SHORT);
 
@@ -74,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
                         Toast.makeText(MainActivity.this, "-180", Toast.LENGTH_SHORT);
 
-                    }
+                    }  */
 
 
 
